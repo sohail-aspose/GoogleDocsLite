594ad198491a1558f1cddf89ac57d4cfe8cb4970
SOHAIL <msohailismail@gmail.com>
2020-06-29 22:56:44 +0500

    Download Content As Word Document

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -4,12 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using GroupDocs.Editor;
+using GroupDocs.Editor.Formats;
 using GroupDocs.Editor.Options;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Microsoft.Net.Http.Headers;
 
 namespace GoogleDocsLite.Pages
 {
@@ -21,7 +23,10 @@ namespace GoogleDocsLite.Pages
         [BindProperty]
         public IFormFile UploadedDocument { get; set; }
 
+        [BindProperty]
         public string DocumentContent { get; set; }
+
+        static volatile string UploadedDocumentPath;
 
         public IndexModel(ILogger<IndexModel> logger, IWebHostEnvironment hostingEnvironment)
         {
@@ -39,13 +44,16 @@ namespace GoogleDocsLite.Pages
             var filePath = Path.Combine(projectRootPath, UploadedDocument.FileName);
             UploadedDocument.CopyTo(new FileStream(filePath, FileMode.Create));
 
-            ShowDocumentContentInTextEditor(filePath);
+            // Retain the path of uploaded document between sessions.
+            UploadedDocumentPath = filePath;
+
+            ShowDocumentContentInTextEditor();
         }
 
-        private void ShowDocumentContentInTextEditor(string filePath)
+        private void ShowDocumentContentInTextEditor()
         {
             WordProcessingLoadOptions loadOptions = new WordProcessingLoadOptions();
-            Editor editor = new Editor(filePath, delegate { return loadOptions; }); //passing path and load options (via delegate) to the constructor
+            Editor editor = new Editor(UploadedDocumentPath, delegate { return loadOptions; }); //passing path and load options (via delegate) to the constructor
             EditableDocument document = editor.Edit(new WordProcessingEditOptions()); //opening document for editing with format-specific edit options
 
             DocumentContent = document.GetBodyContent(); //document.GetContent();
@@ -53,6 +61,32 @@ namespace GoogleDocsLite.Pages
 
             //string embeddedHtmlContent = document.GetEmbeddedHtml();
             //Console.WriteLine("EmbeddedHTMLContent: " + embeddedHtmlContent);
+        }
+
+        public FileResult OnPostDownloadDocument()
+        {
+            // Editor object is referencing to the document initially uploaded for editing.
+            WordProcessingLoadOptions loadOptions = new WordProcessingLoadOptions();
+            Editor editor = new Editor(UploadedDocumentPath, delegate { return loadOptions; });
+
+            // <html>, <head> and <body> tags are missing in the HTML string stored in DocumentContent, so we are manually adding it.
+            string completeHTML = "<!DOCTYPE html><html><head><title></title></head><body>" + DocumentContent + "</body></html>";
+            EditableDocument document = EditableDocument.FromMarkup(completeHTML, null);
+
+            // Path to the output document        
+            var projectRootPath = Path.Combine(_hostingEnvironment.ContentRootPath, "DownloadedDocuments");
+            var outputPath = Path.Combine(projectRootPath, Path.GetFileName(UploadedDocumentPath));
+
+            // Save the Word Document at the outputPath 
+            WordProcessingSaveOptions saveOptions = new WordProcessingSaveOptions(WordProcessingFormats.Docx);
+            editor.Save(document, outputPath, saveOptions);
+
+            // Return the Word Document as response to the User
+            var bytes = System.IO.File.ReadAllBytes(outputPath);
+            return new FileContentResult(bytes, new MediaTypeHeaderValue("application/vnd.openxmlformats-officedocument.wordprocessingml.document"))
+            {
+                FileDownloadName = Path.GetFileName(UploadedDocumentPath)
+            };
         }
     }
 }
